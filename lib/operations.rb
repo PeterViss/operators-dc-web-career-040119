b5d4645e68dbc46293b0a01c275f1004d4644299
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-19 01:33:35 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,7 +11,14 @@ end
 
 
 def not_safe?(speed)
+	if speed > 60
+		true
+	elsif speed < 40
+		true
+	elsif speed > 40 && speed < 60
+		false
 speed > 60 ? true : false
 speed < 40 ? true : false
 speed > 40 & speed < 60 ? true : false
+end
 end
